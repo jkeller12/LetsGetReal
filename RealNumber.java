5c364e82416649872a8b8d40141509f4756b7688
jkeler12 <jkller1@stuy.edu>
2020-11-20 18:17:38 -0500

    Equals compiles

--- RealNumber.java
+++ RealNumber.java
@@ -12,14 +12,21 @@ public class RealNumber{
   public String toString(){
     return ""+getValue();
   }
+
   //---------ONLY EDIT BELOW THIS LINE------------
 
   /*
   *Return true when the values are within 0.001% of eachother.
   *Special case: if one is exactly zero, the other must be exactly zero.
   */
+  final  double EPSILON = (value*.001)/100;
   public boolean equals(RealNumber other){
-    return true;
+    if (other.getValue() == 0 || value == 0)
+    {
+      return other.getValue() == value? true: Math.abs(other.getValue() - value) == 0; 
+    }
+    return other.getValue() == value? true : Math.abs(other.getValue() - value) < EPSILON;
+    //return false;
   }
 
   /*
